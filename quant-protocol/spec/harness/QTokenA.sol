d116422644e2dc6fcd9982f562530cc420f2607c
0xca11 <0xca11@protonmail.com>
2022-03-16 21:59:02 +0700

    feat: add the quant-protocol codebase

---
+++ quant-protocol/spec/harness/QTokenA.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.12;
+
+import "./QTokenCore.sol";
+
+contract QTokenA is QTokenCore {
+    constructor(
+        address _quantConfig,
+        address _underlyingAsset,
+        address _strikeAsset,
+        address _oracle,
+        uint256 _strikePrice,
+        uint256 _expiryTime,
+        bool _isCall
+    )
+        public
+        QTokenCore(
+            _quantConfig,
+            _underlyingAsset,
+            _strikeAsset,
+            _oracle,
+            _strikePrice,
+            _expiryTime,
+            _isCall
+        )
+    {}
+}
---
+++ quant-protocol/spec/harness/QTokenCore.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.12;
+
+import "../../contracts/options/QToken.sol";
+
+contract QTokenCore is QToken {
+    constructor(
+        address _quantConfig,
+        address _underlyingAsset,
+        address _strikeAsset,
+        address _oracle,
+        uint256 _strikePrice,
+        uint256 _expiryTime,
+        bool _isCall
+    )
+        public
+        QToken(
+            _quantConfig,
+            _underlyingAsset,
+            _strikeAsset,
+            _oracle,
+            _strikePrice,
+            _expiryTime,
+            _isCall
+        )
+    {}
+
+    function _assetSymbol(address _quantConfig, address _asset)
+        internal
+        view
+        override
+        returns (string memory symbol)
+    {}
+
+    function _qTokenName(
+        address _quantConfig,
+        address _underlyingAsset,
+        address _strikeAsset,
+        uint256 _strikePrice,
+        uint256 _expiryTime,
+        bool _isCall
+    ) internal view override returns (string memory tokenName) {}
+
+    function _qTokenSymbol(
+        address _quantConfig,
+        address _underlyingAsset,
+        address _strikeAsset,
+        uint256 _strikePrice,
+        uint256 _expiryTime,
+        bool _isCall
+    ) internal view override returns (string memory tokenSymbol) {}
+
+    function _getOptionType(bool _isCall)
+        internal
+        pure
+        override
+        returns (string memory chr, string memory str)
+    {}
+
+    function _displayedStrikePrice(uint256 _strikePrice, address _strikeAsset)
+        internal
+        pure
+        override
+        returns (string memory strikePrice)
+    {}
+
+    function _uintToChars(uint256 _number)
+        internal
+        pure
+        override
+        returns (string memory chars)
+    {}
+
+    function _slice(
+        string memory _s,
+        uint256 _start,
+        uint256 _end
+    ) internal pure override returns (string memory slice) {}
+
+    function _getMonth(uint256 _month)
+        internal
+        pure
+        override
+        returns (string memory monthAbbrev, string memory monthName)
+    {}
+}
